4d213f20d92726430f2da07f24177d708d0b4cbe
ugadavid <david.subileau@etu.univ-grenoble-alpes.fr>
2024-11-07 21:50:51 +0100

    Add last line

---
+++ docs/scripts/popup.js
@@ -0,0 +1,8 @@
+function openPopup(url) {
+  // Ouvre une nouvelle fenêtre avec une taille définie
+  window.open(
+    url,  // URL de la page de contact (peut être un autre fichier HTML)
+      'popupWindow',    // Nom de la fenêtre
+      'width=900,height=900,left=200,top=100' // Taille et position de la fenêtre
+  );
+}
